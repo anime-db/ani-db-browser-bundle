c405677cdafc77cf2ccc349d7a3122bcd960226b
Peter Gribanov <info@peter-gribanov.ru>
2014-07-08 12:43:25 +0400

    add browser test

---
+++ tests/Service/BrowserTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\AniDbBrowserBundle\Service\Tests;
+
+use AnimeDb\Bundle\AniDbBrowserBundle\Service\Browser;
+
+/**
+ * Test browser
+ *
+ * @package AnimeDb\Bundle\AniDbBrowserBundle\Service\Tests
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class BrowserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Host
+     *
+     * @var string
+     */
+    protected $host = 'host';
+
+    /**
+     * API path prefix
+     *
+     * @var string
+     */
+    protected $api_prefix = 'api_prefix';
+
+    /**
+     * API client
+     *
+     * @var string
+     */
+    protected $api_client = 'api_client';
+
+    /**
+     * API clientver
+     *
+     * @var string
+     */
+    protected $api_clientver = 'api_clientver';
+
+    /**
+     * API protover
+     *
+     * @var string
+     */
+    protected $api_protover = 'api_protover';
+
+    /**
+     * Image prefix
+     *
+     * @var string
+     */
+    protected $image_prefix = 'image_prefix';
+
+    /**
+     * Browser
+     *
+     * @var \AnimeDb\Bundle\AniDbBrowserBundle\Service\Browser
+     */
+    protected $browser;
+
+    /**
+     * Client
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $client;
+
+    /**
+     * (non-PHPdoc)
+     * @see PHPUnit_Framework_TestCase::setUp()
+     */
+    public function setUp()
+    {
+        $this->client = $this
+            ->getMockBuilder('\Guzzle\Http\Client')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->browser = new Browser(
+            $this->client,
+            $this->host,
+            $this->api_prefix,
+            $this->api_client,
+            $this->api_clientver,
+            $this->api_protover,
+            $this->image_prefix
+        );
+    }
+
+    /**
+     * Test get image url
+     */
+    public function testGetImageUrl()
+    {
+        $this->assertEquals($this->image_prefix.'foo', $this->browser->getImageUrl('foo'));
+    }
+}
