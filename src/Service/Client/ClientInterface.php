58bc598518cbdcdb9fcd2bcb20b7770d72c2a84c
Peter Gribanov <wcode404@gmail.com>
2016-06-28 17:01:48 +0300

    create client interface

---
+++ src/Service/Client/ClientInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+namespace AnimeDb\Bundle\AniDbBrowserBundle\Service\Client;
+
+interface ClientInterface
+{
+    /**
+     * @param int $timeout
+     *
+     * @return ClientInterface
+     */
+    public function setTimeout($timeout);
+
+    /**
+     * @param string $proxy
+     *
+     * @return ClientInterface
+     */
+    public function setProxy($proxy);
+
+    /**
+     * @param string $request
+     * @param array $params
+     *
+     * @return string
+     */
+    public function get($request, array $params = []);
+}
