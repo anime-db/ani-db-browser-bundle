a302ce052016a2a6de5fcb817921b14999eaf0fa
Peter Gribanov <wcode404@gmail.com>
2016-06-29 16:34:32 +0300

    create configuration for AniDbBrowserBundle

--- src/DependencyInjection/AnimeDbAniDbBrowserExtension.php
+++ src/DependencyInjection/AnimeDbAniDbBrowserExtension.php
@@ -24,5 +24,51 @@ class AnimeDbAniDbBrowserExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('services.yml');
+
+        $config = $this->processConfiguration(new Configuration(), $configs);
+        $config = $this->mergeDefaultConfig($config);
+
+        $container
+            ->getDefinition('anime_db.ani_db.browser.client.guzzle.request_configurator')
+            ->addMethodCall('setAppVersion', $config['app']['version'])
+            ->addMethodCall('setAppClient', $config['app']['client'])
+            ->addMethodCall('setAppCode', $config['app']['code']);
+
+        $container->setAlias('anime_db.ani_db.browser.client', $this->getRealServiceName($config['client']));
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function getRealServiceName($name)
+    {
+        if (in_array($name, ['cache', 'guzzle'])) {
+            return 'anime_db.ani_db.browser.client.'.$name;
+        }
+
+        return $name;
+    }
+
+    /**
+     * @param array $config
+     *
+     * @return array
+     */
+    protected function mergeDefaultConfig(array $config)
+    {
+        $config = array_merge([
+            'client' => 'cache',
+            'app' => [],
+        ], $config);
+
+        $config['app'] = array_merge([
+            'version' => 0,
+            'client' => '',
+            'code' => '',
+        ], $config['app']);
+
+        return $config;
     }
 }
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * AnimeDb package.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+namespace AnimeDb\Bundle\AniDbBrowserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Config tree builder.
+     *
+     * Example config:
+     *
+     * anime_db_ani_db_browser:
+     *     client: 'cache'
+     *     app:
+     *         version: 2
+     *         client: 'animedbplugin'
+     *         code: 'api-team-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $tree_builder = new TreeBuilder();
+        $tree_builder
+            ->root('anime_db_ani_db_browser')
+                ->children()
+                    ->scalarNode('client')
+                        ->cannotBeEmpty()
+                        ->defaultValue('cache')
+                    ->end()
+                    ->arrayNode('app')
+                        ->integerNode('version')
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('client')
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('code')
+                            ->cannotBeEmpty()
+                        ->end()
+                    ->end()
+                ->end();
+
+        return $tree_builder;
+    }
+}
