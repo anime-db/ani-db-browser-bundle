270aebab041101b63eb04289447fbd9153485281
Peter Gribanov <wcode404@gmail.com>
2016-06-29 17:13:00 +0300

    correct test AnimeDbAniDbBrowserExtension

--- tests/DependencyInjection/AnimeDbAniDbBrowserExtensionTest.php
+++ tests/DependencyInjection/AnimeDbAniDbBrowserExtensionTest.php
@@ -10,15 +10,118 @@ namespace AnimeDb\Bundle\AniDbBrowserBundle\Tests\DependencyInjection;
 
 use AnimeDb\Bundle\AniDbBrowserBundle\DependencyInjection\AnimeDbAniDbBrowserExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class AnimeDbAniDbBrowserExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testLoad()
+    /**
+     * @return array
+     */
+    public function getConfigClients()
     {
-        /* @var $builder \PHPUnit_Framework_MockObject_MockObject|ContainerBuilder */
-        $builder = $this->getMock(ContainerBuilder::class);
+        return [
+            ['', 'anime_db.ani_db.browser.client.cache'],
+            ['cache', 'anime_db.ani_db.browser.client.cache'],
+            ['guzzle', 'anime_db.ani_db.browser.client.guzzle'],
+            ['custom_client', 'custom_client'],
+        ];
+    }
+
+    /**
+     * @dataProvider getConfigClients
+     *
+     * @param string $client
+     * @param string $service
+     */
+    public function testLoad($client, $service)
+    {
+        $config = [
+            'anime_db_ani_db_browser' => [
+                'app' => [
+                    'version' => 2,
+                    'client' => 'animedbplugin',
+                    'code' => 'api-team-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                ],
+            ],
+        ];
+
+        if ($client) {
+            $config['anime_db_ani_db_browser']['client'] = $client;
+        }
+
+        $i = -1;
+        $configurator = $this->getMock(Definition::class);
+        $configurator
+            ->expects($this->at(++$i))
+            ->method('addMethodCall')
+            ->with('setAppVersion', [$config['anime_db_ani_db_browser']['app']['version']])
+            ->will($this->returnSelf());
+        $configurator
+            ->expects($this->at(++$i))
+            ->method('addMethodCall')
+            ->with('setAppClient', [$config['anime_db_ani_db_browser']['app']['client']])
+            ->will($this->returnSelf());
+        $configurator
+            ->expects($this->at(++$i))
+            ->method('addMethodCall')
+            ->with('setAppCode', [$config['anime_db_ani_db_browser']['app']['code']])
+            ->will($this->returnSelf());
+
+
+        /* @var $container \PHPUnit_Framework_MockObject_MockObject|ContainerBuilder */
+        $container = $this->getMock(ContainerBuilder::class);
+        $container
+            ->expects($this->once())
+            ->method('getDefinition')
+            ->with('anime_db.ani_db.browser.client.guzzle.request_configurator')
+            ->will($this->returnValue($configurator));
+        $container
+            ->expects($this->once())
+            ->method('setAlias')
+            ->with(
+                'anime_db.ani_db.browser.client',
+                $service
+            );
 
         $di = new AnimeDbAniDbBrowserExtension();
-        $di->load([], $builder);
+        $di->load($config, $container);
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfigApps()
+    {
+        return [
+            [[]],
+            [['version' => 1]],
+            [['client' => 'animedbplugin']],
+            [['code' => 'api-team-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']],
+            [['version' => 1, 'client' => 'animedbplugin']],
+            [['version' => 1, 'code' => 'api-team-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']],
+            [['client' => 'animedbplugin', 'code' => 'api-team-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']],
+        ];
+    }
+
+    /**
+     * @dataProvider getConfigApps
+     * @expectedException \RuntimeException
+     *
+     * @param array $app
+     */
+    public function testLoadFailed(array $app)
+    {
+
+        $config = ['anime_db_ani_db_browser' => []];
+
+        if ($app) {
+            $config['anime_db_ani_db_browser']['app'] = $app;
+        }
+
+        /* @var $container \PHPUnit_Framework_MockObject_MockObject|ContainerBuilder */
+        $container = $this->getMock(ContainerBuilder::class);
+
+        $di = new AnimeDbAniDbBrowserExtension();
+        $di->load($config, $container);
     }
 }
