3cf7a4da17a312c9d9290b0397a04fca7e00d7f6
Peter Gribanov <wcode404@gmail.com>
2017-07-18 13:10:15 +0300

    not cache responses

--- src/DependencyInjection/AnimeDbAniDbBrowserExtension.php
+++ src/DependencyInjection/AnimeDbAniDbBrowserExtension.php
@@ -24,7 +24,6 @@ class AnimeDbAniDbBrowserExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('parameters.yml');
         $loader->load('services.yml');
 
         $config = $this->processConfiguration(new Configuration(), $configs);
